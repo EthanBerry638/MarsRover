a41b87552167db95e9ce459455300d2c3a76fe8d
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 16:21:10 +0100

    test now failing

--- MarsRover/MarsRover.Tests/RoverTests.cs
+++ MarsRover/MarsRover.Tests/RoverTests.cs
@@ -1,10 +1,23 @@
-﻿using NUnit;
+﻿using MarsRover.Console.Data;
+using MarsRover.Console.Directions;
+using MarsRover.Console.Rover;
+
+using NUnit;
+
+namespace MarsRover.Tests;
 
 public class RoverTests
 {
     [Test]
     public void Rotate_ShouldReturnEast_WhenGivenARotaionOfRightFromNorth()
     {
+        Position position = new Position(3, 3, CompassDirection.N);
+        var rover = new Rover(position);
+        var rotation = Instruction.R;
+        var expected = CompassDirection.E;
 
+        CompassDirection result = rover.Rotate(rotation);
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
