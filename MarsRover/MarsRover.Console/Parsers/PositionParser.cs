89578f3309a4c07ed1c815c062088221b6861011
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 14:37:29 +0100

    passing for some but not for others

--- MarsRover/MarsRover.Console/Parsers/PositionParser.cs
+++ MarsRover/MarsRover.Console/Parsers/PositionParser.cs
@@ -11,16 +11,13 @@ namespace MarsRover.Console.Parsers
         public static string RawPositionParser(string rawPosition)
         {
             if (string.IsNullOrEmpty(rawPosition)) return "";
+  
+            string[] parts = rawPosition.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            parts[2] = parts[2].ToUpper();
 
-            if (rawPosition.Length == 5)
+            if (int.TryParse(parts[0], out int value) && int.TryParse(parts[1], out int value2))
             {
-                string[] parts = rawPosition.Split(' ');
-                parts[2] = parts[2].ToUpper();
-
-                if (int.TryParse(parts[0], out int value) && int.TryParse(parts[1], out int value2))
-                {
-                    return $"{parts[0]} {parts[1]} {parts[2]}";
-                }
+                return $"{parts[0]} {parts[1]} {parts[2]}";
             }
 
             return "";
