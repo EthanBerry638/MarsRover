8e6096055053d77cfb8ac38cdf8e7042ec75f490
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 14:54:21 +0100

    new test. already passing...

--- MarsRover/MarsRover.Tests/PositionParserTests.cs
+++ MarsRover/MarsRover.Tests/PositionParserTests.cs
@@ -103,4 +103,15 @@ public class PositionParserTests
 
         Assert.That(result, Is.EqualTo(expected));
     }
+
+    [Test]
+    public void RawPositionParser_ShouldReturnEmptyString_WhenGivenANumberThatIsNotAnInteger()
+    {
+        string input = "1.2 3 W";
+        string expected = "";
+
+        string result = PositionParser.RawPositionParser(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
 }
