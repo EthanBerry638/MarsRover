f14d27d763c6d862c4db4a2e235b6b594347ec3b
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 09:52:38 +0100

    added position record

---
+++ MarsRover/MarsRover.Console/Rover/Position.cs
@@ -0,0 +1,11 @@
+﻿using MarsRover.Console.Directions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Console.Rover
+{
+    public record Position (int x, int y, CompassDirections facing);
+}
