73ed8d79eccd00f1a56994baac5ef4d455efca88
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 15:52:02 +0100

    tested in program.cs. works as expected!

--- MarsRover/MarsRover.Console/Program.cs
+++ MarsRover/MarsRover.Console/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MarsRover.Console.Data;
+using MarsRover.Console.Directions;
+using MarsRover.Console.Parsers;
+
+string rawInstruction = InstructionParser.ParseInstruction("LMLMLMLMM");
+List<Instruction> instructions = InstructionParser.GetInstructions(rawInstruction);
+
+foreach (Instruction instruction in instructions) Console.WriteLine(instruction);
+
+int[] rawPlateau = PlateauParser.ParseRawPlateau("5 5");
+PlateauSize plateau = PlateauParser.GetPlateauSize(rawPlateau);
+
+Console.WriteLine("\n" + plateau);
+
+string rawPosition = PositionParser.RawPositionParser("3 3 E");
+Position position = PositionParser.GetPosition(rawPosition, plateau);
+
+Console.WriteLine("\n" + position);
