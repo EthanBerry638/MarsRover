5c8eb6a29f5bf27f15e69159ffb8ed822e5ecbda
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 16:38:03 +0100

    added new failing test

--- MarsRover/MarsRover.Tests/RoverTests.cs
+++ MarsRover/MarsRover.Tests/RoverTests.cs
@@ -35,11 +35,24 @@ public class RoverTests
     }
 
     [Test]
-    public void Rotate_ShouldReturnOriginalDirection_WhenGivenARotationInstructionOfM()
+    public void Rotate_ShouldReturnOriginalDirection_WhenGivenARotationOfM()
     {
         Position position = new Position(3, 3, CompassDirection.N);
         var rover = new Rover(position);
         var rotation = Instruction.M;
+        var expected = CompassDirection.N;
+
+        CompassDirection result = rover.Rotate(rotation);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Rotate_ShouldWrapAroundToNorth_WhenGivenARotationOfRightFromWest()
+    {
+        Position position = new Position(3, 3, CompassDirection.W);
+        var rover = new Rover(position);
+        var rotation = Instruction.R;
         var expected = CompassDirection.N;
 
         CompassDirection result = rover.Rotate(rotation);
