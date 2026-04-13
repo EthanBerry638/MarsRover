f72f7baefd92042e74204cbd1a4092a694138ba0
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 16:29:52 +0100

    added failing test for Instruction M

--- MarsRover/MarsRover.Tests/RoverTests.cs
+++ MarsRover/MarsRover.Tests/RoverTests.cs
@@ -33,4 +33,17 @@ public class RoverTests
 
         Assert.That(result, Is.EqualTo(expected));
     }
+
+    [Test]
+    public void Rotate_ShouldReturnOriginalDirection_WhenGivenARotationInstructionOfM()
+    {
+        Position position = new Position(3, 3, CompassDirection.N);
+        var rover = new Rover(position);
+        var rotation = Instruction.M;
+        var expected = CompassDirection.W;
+
+        CompassDirection result = rover.Rotate(rotation);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
 }
