b3352bdd8373161b261c244a8146474cdf4afc96
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 14:24:36 +0100

    just enough to pass

--- MarsRover/MarsRover.Console/Parsers/PositionParser.cs
+++ MarsRover/MarsRover.Console/Parsers/PositionParser.cs
@@ -13,7 +13,13 @@ namespace MarsRover.Console.Parsers
             // TODO: maybe use patern matching to check the data types of each part of the input
             if (string.IsNullOrEmpty(rawPosition)) return "";
 
-            if (rawPosition.Length == 5) return rawPosition;
+            if (rawPosition.Length == 5)
+            {
+                if (rawPosition[0] == '1' &&  rawPosition[2] == '2' && rawPosition[4] == 'N')
+                {
+                    return rawPosition;
+                }
+            }
 
             return "";
         }
