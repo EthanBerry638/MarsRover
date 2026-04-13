9f9e978d3c4be986300494fc70267574c7148b82
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 14:06:09 +0100

    added failing test (returns nothing so doesnt build though

--- MarsRover/MarsRover.Tests/PositionParserTests.cs
+++ MarsRover/MarsRover.Tests/PositionParserTests.cs
@@ -1,7 +1,18 @@
-﻿using NUnit.Framework;
+﻿using MarsRover.Console.Parsers;
+using NUnit.Framework;
 
 namespace MarsRover.Tests;
 
 public class PositionParserTests
 {
+    [Test]
+    public void RawPositionParser_ShouldReturnEmptyString_WhenGivenNull()
+    {
+        string? input = null;
+        string expected = "";
+
+        string result = PositionParser.RawPositionParser(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
 }
