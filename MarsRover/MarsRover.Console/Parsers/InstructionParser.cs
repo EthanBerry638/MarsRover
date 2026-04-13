ea4d3798de3d0286ad8fdeb05e07af470d3762fc
Ethan Berry <ethanberry670@gmail.com>
2026-04-13 10:34:51 +0100

    now passing test

--- MarsRover/MarsRover.Console/Parsers/InstructionParser.cs
+++ MarsRover/MarsRover.Console/Parsers/InstructionParser.cs
@@ -11,7 +11,7 @@ namespace MarsRover.Console.Parsers
     {
         public static string ParseInstruction(string rawInstruction)
         {
-            if (rawInstruction == Instruction.L.ToString())
+            if (rawInstruction.ToUpper() == Instruction.L.ToString())
             {
                 return "L";
             }
