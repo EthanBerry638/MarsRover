9a08ed71a965c37259bb904755f642db554789fc
Ethan Berry <ethanberry670@gmail.com>
2026-04-14 10:45:53 +0100

    added new failing test

--- MarsRover/MarsRover.Tests/PlateauTests.cs
+++ MarsRover/MarsRover.Tests/PlateauTests.cs
@@ -24,5 +24,21 @@ namespace MarsRover.Tests
 
             Assert.That(result, Is.EqualTo(expected));
         }
+
+        [Test]
+        public void IsBordering_ShouldReturnFalse_WhenPositionOfRoverIsNotExceedingPlateauSize()
+        {
+            Position testPosition = new(3, 3, CompassDirection.N);
+            Rover testRover = new(testPosition);
+
+            PlateauSize testPlateauSize = new(5, 5);
+            Plateau testPlateau = new(testPlateauSize);
+
+            bool expected = false;
+
+            bool result = testPlateau.IsBordering(testRover);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
     }
 }
