c068b6d75381c230ab97aec262b052b6c83b28cd
Ethan Berry <ethanberry670@gmail.com>
2026-04-14 10:28:10 +0100

    added test

--- MarsRover/MarsRover.Tests/RoverTests.cs
+++ MarsRover/MarsRover.Tests/RoverTests.cs
@@ -59,4 +59,17 @@ public class RoverTests
 
         Assert.That(result, Is.EqualTo(expected));
     }
+
+    [Test]
+    public void Rotate_ShouldWrapAroundToWest_WhenRotatedLeftFromNorth()
+    {
+        Position position = new Position(3, 3, CompassDirection.N);
+        var rover = new Rover(position);
+        var rotation = Instruction.L;
+        var expected = CompassDirection.W;
+
+        CompassDirection result = rover.Rotate(rotation);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
 }
