0337d63d217e19c3eb166aa7c3c20373b79e1a7a
Ethan Berry <ethanberry670@gmail.com>
2026-04-26 16:39:19 +0100

    added new failing test

---
+++ MarsRover/MarsRover.Tests/RoverNameParserTest.cs
@@ -0,0 +1,18 @@
+﻿using MarsRover.Console.Parsers;
+
+namespace MarsRover.Tests
+{
+    public class RoverNameParserTest
+    {
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("              ")]
+        public void RoverNameParser_ShouldReturnNull_WhenGivenNullOrEmptyString(string? input)
+        {
+            string? result = RoverNameParser.GetRawName(input!);
+
+            Assert.That(result, Is.EqualTo(null));
+        }
+    }
+}
