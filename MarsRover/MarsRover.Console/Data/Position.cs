e8837ebeb6b299adcfff792c094d3e3f1d618b90
Ethan Berry <ethanberry670@gmail.com>
2026-04-28 22:13:44 +0100

    refactor: now using primary constructor

--- MarsRover/MarsRover.Console/Data/Position.cs
+++ MarsRover/MarsRover.Console/Data/Position.cs
@@ -2,18 +2,11 @@
 
 namespace MarsRover.Console.Data
 {
-    public class Position
+    public class Position (int x, int y, CompassDirection facing)
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public CompassDirection Facing { get; set; }
-
-        public Position(int x, int y, CompassDirection facing)
-        {
-            X = x;
-            Y = y;
-            Facing = facing;
-        }
+        public int X { get; set; } = x;
+        public int Y { get; set; } = y;
+        public CompassDirection Facing { get; set; } = facing;
 
         public override bool Equals(object? obj)
         {
